8e23f8f54d80e7c27d3c28f976c79ccf322fc9d8
Sascha Kratky <skratky@gmail.com>
2010-10-22 19:03:07 +0200

    boost: add optional support for MPI and universal build

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -1,9 +1,26 @@
 require 'formula'
 
+def build_mpi?; ARGV.include? "--mpi"; end
+def build_universal?; ARGV.include? "--universal"; end
+
 class Boost <Formula
   homepage 'http://www.boost.org'
   url 'http://downloads.sourceforge.net/project/boost/boost/1.44.0/boost_1_44_0.tar.bz2'
   md5 'f02578f5218f217a9f20e9c30e119c6a'
+
+  def options
+    [
+      ['--mpi', 'Build Message Passing Interface library.'],
+      ['--universal', 'Build as a Universal binary.'],
+    ]
+  end
+
+  def patches
+    # PPC needs patch
+    if build_universal?
+      DATA
+    end
+  end
 
   def install
     fails_with_llvm "LLVM-GCC causes errors with dropped arguments to "+
@@ -30,16 +47,47 @@ class Boost <Formula
     # Force boost to compile using the GCC 4.2 compiler
     open("user-config.jam", "a") do |file|
       file.write "using darwin : : #{ENV['CXX']} ;\n"
+      file.write "using mpi ;\n" if build_mpi?
     end
 
-    # we specify libdir too because the script is apparently broken
     system "./bootstrap.sh", "--prefix=#{prefix}", "--libdir=#{lib}"
-    system "./bjam", "--prefix=#{prefix}",
-                     "--libdir=#{lib}",
-                     "-j#{Hardware.processor_count}",
-                     "--layout=tagged",
-                     "--user-config=user-config.jam",
-                     "threading=multi",
-                     "install"
+    # we specify libdir too because the script is apparently broken
+    args = ["--prefix=#{prefix}",
+            "--libdir=#{lib}",
+            "-j#{Hardware.processor_count}",
+            "--layout=tagged",
+            "--user-config=user-config.jam",
+            "threading=multi"]
+    args << "architecture=combined" << "address-model=32_64" if build_universal?
+    args << "install"
+    system "./bjam", *args
   end
 end
+
+__END__
+diff --git a/boost/archive/basic_binary_iarchive.hpp b/boost/archive/basic_binary_iarchive.hpp
+index d756926..bd42766 100644
+--- a/boost/archive/basic_binary_iarchive.hpp
++++ b/boost/archive/basic_binary_iarchive.hpp
+@@ -71,7 +71,7 @@ public:
+ 
+     // include these to trap a change in binary format which
+     // isn't specifically handled
+-    BOOST_STATIC_ASSERT(sizeof(tracking_type) == sizeof(char));
++    BOOST_STATIC_ASSERT(sizeof(tracking_type) == sizeof(bool));
+     // upto 32K classes
+     BOOST_STATIC_ASSERT(sizeof(class_id_type) == sizeof(int_least16_t));
+     BOOST_STATIC_ASSERT(sizeof(class_id_reference_type) == sizeof(int_least16_t));
+diff --git a/boost/archive/basic_binary_oarchive.hpp b/boost/archive/basic_binary_oarchive.hpp
+index f20471a..9465c1d 100644
+--- a/boost/archive/basic_binary_oarchive.hpp
++++ b/boost/archive/basic_binary_oarchive.hpp
+@@ -76,7 +76,7 @@ public:
+ 
+     // include these to trap a change in binary format which
+     // isn't specifically handled
+-    BOOST_STATIC_ASSERT(sizeof(tracking_type) == sizeof(char));
++    BOOST_STATIC_ASSERT(sizeof(tracking_type) == sizeof(bool));
+     // upto 32K classes
+     BOOST_STATIC_ASSERT(sizeof(class_id_type) == sizeof(int_least16_t));
+     BOOST_STATIC_ASSERT(sizeof(class_id_reference_type) == sizeof(int_least16_t));
