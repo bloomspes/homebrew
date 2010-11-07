0e51b541c9875d8ce6869043690de18badb7c149
Sascha Kratky <skratky@gmail.com>
2010-11-07 21:00:56 +0100

    update cmake to 2.8.3

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Cmake <Formula
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.2.tar.gz'
-  md5 '8c967d5264657a798f22ee23976ff0d9'
+  url 'http://www.cmake.org/files/v2.8/cmake-2.8.3.tar.gz'
+  md5 'a76a44b93acf5e3badda9de111385921'
   homepage 'http://www.cmake.org/'
 
   def patches
     # CMAKE_OSX_ARCHITECTURES quoting bug. See: http://www.vtk.org/Bug/view.php?id=11244
     # Not needed with CMake 2.8.3 and above.
-    [ "http://cmake.org/gitweb?p=cmake.git;a=patch;h=a8ded533",
-      "http://cmake.org/gitweb?p=cmake.git;a=patch;h=0790af3b" ]
+#    [ "http://cmake.org/gitweb?p=cmake.git;a=patch;h=a8ded533",
+#      "http://cmake.org/gitweb?p=cmake.git;a=patch;h=0790af3b" ]
   end
 
   def install
@@ -18,7 +18,7 @@ class Cmake <Formula
     # needs an xmlrpc library anyway! It is amazing!
     inreplace 'CMakeLists.txt',
               "# Mention to the user what system libraries are being used.",
-              "SET(CMAKE_USE_SYSTEM_XMLRPC 0)"
+              "SET(CMAKE_USE_SYSTEM_XMLRPC 0)\nSET(CMAKE_USE_SYSTEM_LIBARCHIVE 0)"
 
     system "./bootstrap", "--prefix=#{prefix}",
                           "--system-libs",
