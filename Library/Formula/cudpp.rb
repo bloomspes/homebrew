b2317f6fd14acf6cad82fd4c8c3dc7c1b7e2bcdb
Sascha Kratky <skratky@gmail.com>
2011-09-04 21:22:54 +0200

    new formula: cudpp

---
+++ Library/Formula/cudpp.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Cudpp < Formula
+  url 'http://cudpp.googlecode.com/files/cudpp_src_2.0.zip'
+  homepage 'http://code.google.com/p/cudpp/'
+  md5 '7b4270bfd143621a0dfee28496d66387'
+
+  depends_on 'cmake' => :build
+
+  def install
+    system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+
+end
