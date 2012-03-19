c77a5df3a74fa2d9c07522fa940b66324a24b4db
Sascha Kratky <skratky@gmail.com>
2012-03-19 20:39:11 +0100

    QuantLib 1.2

--- Library/Formula/quantlib.rb
+++ Library/Formula/quantlib.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Quantlib < Formula
-  url 'http://sourceforge.net/projects/quantlib/files/QuantLib/1.1/QuantLib-1.1.tar.gz'
+  url 'http://sourceforge.net/projects/quantlib/files/QuantLib/1.2/QuantLib-1.2.tar.gz'
   homepage 'http://quantlib.org/'
-  md5 'bca1281b64677edab96cc97d2b1a6678'
+  md5 'b7d13feb6c24671e171a28dc1898a0d2'
 
   depends_on 'boost'
 
