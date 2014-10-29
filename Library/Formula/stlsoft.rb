ad5d1933273697d4827832fb6198188580d7a9d3
Sascha Kratky <skratky@gmail.com>
2014-10-29 16:45:34 +0100

    stlsoft 1.9.118

--- Library/Formula/stlsoft.rb
+++ Library/Formula/stlsoft.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Stlsoft < Formula
   homepage 'http://www.stlsoft.org'
-  url 'http://sourceforge.net/projects/stlsoft/files/STLSoft%201.9/1.9.117/stlsoft-1.9.117-hdrs.zip'
-  version '1.9.117'
-  sha1 '6d6236e6c80e7c375c42eb4c584e5625a28512ec'
+  url 'http://sourceforge.net/projects/stlsoft/files/STLSoft%201.9/1.9.118/stlsoft-1.9.118-hdrs.zip'
+  version '1.9.118'
+  sha1 '34353510f5a10bee39401bc4f07161ab34cbe0fc'
 
   depends_on 'dos2unix' => :build
 
