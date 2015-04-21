aa9934b50ae0dcfceb4290ca5f1f454a9ee3e00b
Sascha Kratky <kratky@unisoftwareplus.com>
2015-04-21 10:28:39 +0200

    sshpass 1.05

--- Library/Formula/sshpass.rb
+++ Library/Formula/sshpass.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Sshpass < Formula
-  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.04/sshpass-1.04.tar.gz'
+  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz'
   homepage 'http://sshpass.sourceforge.net/'
-  md5 '87e7c72e319691c5fdf219f6c7effb4a'
+  sha256 'c3f78752a68a0c3f62efb3332cceea0c8a1f04f7cf6b46e00ec0c3000bc8483e'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
