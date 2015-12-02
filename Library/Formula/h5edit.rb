46c1f1c0c02dc7a43abb2b9d6bb6b3070df602c3
Sascha Kratky <skratky@gmail.com>
2015-12-02 20:58:49 +0100

    cleanup h5edit formula

--- Library/Formula/h5edit.rb
+++ Library/Formula/h5edit.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class H5edit < Formula
+  desc "tool for editing HDF5 files"
   homepage "http://www.hdfgroup.org/projects/jpss/h5edit_index.html"
   url "http://www.hdfgroup.org/ftp/HDF5/projects/jpss/h5edit/h5edit-1.3.1.tar.gz"
   sha1 "69a7275b2f7d4ba61f1d3c333f523d2119a78d57"
